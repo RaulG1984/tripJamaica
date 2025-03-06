05a2ccc64908f09742ac3b900e55f3fa2ed60069
carlosluciano1986 <t3-101@hotmail.com>
2025-03-06 21:30:04 +0100

    hola

--- index.php
+++ index.php
@@ -23,6 +23,69 @@
         </form>
     </div>
 
+     <?php
+
+        $query = "SELECT * FROM jamaica";
+        $result = $conn->query($query);
+
+        if ($result->num_rows > 0) {
+    ?>
+            <div class="container mt-3 table-responsive">
+                <table class="table table-hover text-center">
+                    <thead class="table-dark">
+                        <tr>
+                            <th>ID</th>
+                            <th>gasto</th>
+                            <th>base</th>
+                            <th>iva(21%)</th>
+                            <th>monto</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+    <?php
+                $counter = '';
+                while ($row = $result->fetch_assoc()) {
+                    $counter++;
+    ?>
+                        <tr>
+                            <td class="wrap"><?php echo $counter; ?></td>
+                            <td class="wrap"><?php echo $row['gastos']; ?></td>
+                            <td class="wrap"><?php echo $row['monto']-($row['monto']* 0.21); ?></td>
+                            <td class="wrap"><?php echo $row['monto']* 0.21; ?></td>
+                            <td class="wrap"><?php echo $row['monto']; ?></td>
+
+
+
+                        </tr>
+    <?php
+                }
+    ?>
+                    </tbody>
+                </table>
+            </div>
+    <?php
+        }
+        else {
+    ?>
+
+            <div class="container col-md-3 mt-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">No hay Datos</h5>
+                        <h6 class="card-subtitle mb-2 text-body-secondary">Bo se han enc...</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="container mt-3">
+                <img src="banner.jpg" class="img-fluid" alt="No Data">
+            </div>
+    <?php
+        }
+        $conn->close();
+    ?>
+
 
 
 </body>
